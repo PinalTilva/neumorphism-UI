0e5d8768ab42ea8a234cd0a64a0a11ac86b6e0ae
PinalTilva <tilavapm@gmail.com>
2023-01-25 19:23:20 +0530

    changed navigation

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,24 +1,21 @@
 import clsx from "clsx";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Button = (props) => {
   const { text, color } = props;
   return (
     <>
-      <Link to={"/login"}>
-        <button
-          className={clsx(
-            "text-slate-400 min-w-[150px] active:text-blue-400 shadow-neu active:shadow-neu- bg-slate-200 py-1 px-4 rounded-full",
-            {
-              "bg-green-500 text-white active:shadow-neuGreen- active:text-white active:font-semibold":
-                color === "success",
-            }
-          )}
-        >
-          {text}
-        </button>
-      </Link>
+      <button
+        className={clsx(
+          "text-slate-400 min-w-[150px] active:text-blue-400 shadow-neu active:shadow-neu- bg-slate-200 py-1 px-4 rounded-full",
+          {
+            "bg-green-500 text-white active:shadow-neuGreen- active:text-white active:font-semibold":
+              color === "success",
+          }
+        )}
+      >
+        {text}
+      </button>
     </>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { ComputerIcon } from "../../icons";
 import { Button } from "../../components";
+import { Link } from "react-router-dom";
 const Home = () => {
-  const navigate = useNavigate();
-
   return (
     <React.Fragment>
       <div className="w-6/12 max-w-[300px]">
         <ComputerIcon />
       </div>
-      <Button text="Go to login" />
+      <Link to={"/login"}>
+        <Button text="Go to login" />
+      </Link>
     </React.Fragment>
   );
 };
